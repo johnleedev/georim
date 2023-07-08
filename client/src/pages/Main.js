b0a512832f4abc68ad20b763c53e795247c3bfd3
john <johnlovesyou@naver.com>
2023-07-08 11:58:09 +0900

    230708

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -16,29 +16,6 @@ import { faBuilding, faCity, faHotel, faShop } from "@fortawesome/free-solid-svg
 
 function Main(props) {
 
-  window.addEventListener("wheel", function(e){
-    e.preventDefault();
-  },{passive : false});
-
-  var mHtml = $("html");
-  var page = 1;
-  
-  mHtml.animate({scrollTop : 0}, 10);
-
-  $(window).on("wheel", function(e) {
-    if(mHtml.is(":animated")) return;
-    if(e.originalEvent.deltaY > 0) {
-        if(page == 4) return;
-        page++;
-    } else if(e.originalEvent.deltaY < 0) {
-        if(page == 1) return;
-        page--;
-    }
-    var posTop =(page-1) * $(window).height();
-    mHtml.animate({scrollTop : posTop});
-  })
-
-
   // popup //
   const [showPopup, setShowPopup] = useState('');
   const HOME_VISITED = localStorage.getItem("homeVisited"); // localStorage에 homeVisited 조회
